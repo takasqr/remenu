d650052fe4053a80c1dbeb05a71834299a42faf4
takasqr <95556025+takasqr@users.noreply.github.com>
2021-12-09 00:14:03 +0900

    vuex layout.js

---
+++ src/store/layout.js
@@ -0,0 +1,21 @@
+export default {
+  namespaced: true,
+  state: {
+    drawer: true,
+  },
+  mutations: {
+    toggleDrawer (state) {
+      state.drawer = !state.drawer
+    }
+  },
+  actions: {
+    TOGGLE_DRAWER ({ commit }) {
+      commit('toggleDrawer')
+    }
+  },
+  getters: {
+    DRAWER_STATE (state) {
+      return state.drawer
+    }
+  }
+}
