cbfcd7d0dc3efb612b29b94b9204d5f4f519c7af
takasqr <95556025+takasqr@users.noreply.github.com>
2021-12-11 15:13:02 +0900

    Update src/components/component/NewTask.vue

---
+++ src/components/component/NewTask.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-card rounded="lg">
+      <v-card-text>
+        <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+          @submit="addTask"
+        >
+          <v-text-field
+            v-model="inputTask"
+            :rules="inputTaskRules"
+            :label="this.$t('message.input_new_task')"
+            required
+            validate-on-blur
+            clearable
+          />
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      inputTask: null,
+      valid: true,
+      inputTaskRules: [
+        v => !!v || this.$t('message.input_error_require'),
+        v => (v && v.length <= 50) || '50文字以内で入力してください。'
+      ]
+    }
+  },
+  methods: {
+    addTask (e) {
+      e.preventDefault()
+    }
+  }
+}
+</script>
