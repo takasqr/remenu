1345a0646bd704564dc0fb47ce0cb1d3316e8527
takasqr <95556025+takasqr@users.noreply.github.com>
2021-12-18 23:03:08 +0900

    Update src/components/page/HabitPage.vue

---
+++ src/components/page/HabitPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NewHabit />
+    <HabitList />
+  </div>
+</template>
+
+<script>
+import NewHabit from '@/components/component/NewHabit.vue'
+import HabitList from '@/components/component/HabitList.vue'
+
+export default {
+  components: {
+    NewHabit,
+    HabitList
+  }
+}
+</script>
